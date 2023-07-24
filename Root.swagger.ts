4be68b144bb6094308e783f841eae334b80a4e8b
Yury Marin <yurimar1532@gmail.com>
2023-07-23 21:56:19 -0500

    Actividad15

---
+++ Root.swagger.ts
@@ -0,0 +1,18 @@
+export default {
+
+    /**
+     * @swagger
+     * /:
+     *   get:
+     *     summary: Obtener un mensaje de ejemplo
+     *     responses:
+     *       200:
+     *         description: Mensaje de ejemplo
+     *         content:
+     *           text/plain:
+     *             schema:
+     *               type: string
+     *             example: "¡Hola, este es un mensaje de ejemplo!"
+     */
+
+    }
